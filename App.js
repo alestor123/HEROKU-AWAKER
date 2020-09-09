868413836932daacf4f70cff61e01308fe099460
alestor123 <alestoraldous@gmail.com>
2020-09-09 16:40:00 +0530

    importing axios

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
 var { spawn } = require('child_process'),
-    meow = require('meow'),
+    axios = require('axios');
     url = process.argv[2],
     time = process.argv[3];
