c513e3d58c1a6c7259b07d0458798f83fa1bfb47
alestor123 <alestoraldous@gmail.com>
2020-09-09 17:22:17 +0530

    added comm

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-#! /usr/bin/env node
-
+#! /usr/bin/env node 
+//setting bash
+// importing modules 
 var { spawn } = require('child_process'),
     axios = require('axios');
-    url = process.argv[2],
+    url = process.argv[2], //getting args
 time = process.argv[3];
-if (!time || !url) {
+if (!time || !url) { // checking undifined args
     console.log(`
     Invalid command.
     Usage: heroku-awaker <url> <time:minutes>  
   `);
 }
-else{
+else{ //running this code
 setInterval(
     function(){  
-axios.get(url)
+axios.get(url)//getting url
 .then(function() {
   // handle success
   console.log('awaking');
